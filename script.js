e93c9cb7a8601b564e8ad3ed81cb575eee564b59
Yosef Haim <yosefhaimmalamud@gmail.com>
2024-09-04 09:54:35 +0300

    Some functions in JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const x = 9;
+const y = 4;
+const result = x + y;
+console.log(result);
+const myList = [3, 'hr', 177, false];
+for (i in myList)
+    console.log(myList[i])
+const title = document.getElementsByClassName('my')
+title[0].innerHTML = 'rtert'
