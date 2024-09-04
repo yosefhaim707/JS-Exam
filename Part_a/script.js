2a6d7c9f137cff918ff59f3565191cfd9d3fac1a
Yosef Haim <yosefhaimmalamud@gmail.com>
2024-09-04 10:57:15 +0300

    Exam Part 1

---
+++ Part_a/script.js
@@ -0,0 +1,9 @@
+// exam 1
+let exampleVar = 'I Know JavaScript';
+console.log(exampleVar);
+
+// exam 2
+let exampleVarB = 'Me Not Know JavaScript';
+console.log(exampleVarB);
+exampleVarB = exampleVarB.replace('Me Not', exampleVar[0]);
+console.log(exampleVarB);
